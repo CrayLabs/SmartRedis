3034e30f2e2845aa3ee92a98708d281cf6341f7c
Matt Drozt <drozt@hpe.com>
2022-01-27 11:35:56 -0500

    clone catch2, use single header in release

--- build-scripts/build-catch.sh
+++ build-scripts/build-catch.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Install Catch
-if [[ -f ./catch/catch.hpp ]]; then
+if [[ -f ./catch/single_include/catch2/catch.hpp ]]; then
     echo "Catch has already been download and installed"
 else
     echo "Installing Catch"
     if [[ ! -d "./catch" ]]; then
-        wget https://github.com/catchorg/Catch2/releases/download/v2.13.6/catch.hpp -P catch
+        git clone https://github.com/catchorg/Catch2.git catch
+        cd catch
+        git checkout tags/v2.13.6
+        cd ..
     else
         echo "Catch downloaded"
     fi
