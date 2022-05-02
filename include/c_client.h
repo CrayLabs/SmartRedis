376608f86794d35d48f9fc816a1db190672bee16
Andrew Shao <Andrew.Shao@hpe.com>
2022-05-02 16:34:49 -0700

    Add signatures and docs for C-client multigpu
    The C-client now has the multigpu-function documentation and signatures.
    The methods themselves still need to be implemented.

--- include/c_client.h
+++ include/c_client.h
@@ -362,6 +362,61 @@ SRError set_model_from_file(void* c_client,
                             const size_t n_outputs);
 
 /*!
+*   \brief Set a model from file in the database for future
+*          execution in a multi-GPU system
+*   \details The final model key used to store the model
+*            may be formed by applying a prefix to the supplied
+*            name. Similarly, the tensor names in the
+*            input and output nodes for TF models may be prefixed.
+*            See set_data_source(), use_model_ensemble_prefix(), and
+*            use_tensor_ensemble_prefix() for more details
+*   \param c_client The client object to use for communication
+*   \param name The name to associate with the model
+*   \param name_length The length of the name string,
+*                      excluding null terminating character
+*   \param model_file The source file for the model
+*   \param model_file_length The length of the model_file string,
+*                            excluding null terminating character
+*   \param backend The name of the backend (TF, TFLITE, TORCH, ONNX)
+*   \param backend_length The length of the backend string,
+*                        excluding null terminating character
+*   \param first_gpu the first gpu (zero-based) to use with the model
+*   \param num_gpus the number of gpus to use with the model
+*   \param batch_size The batch size for model execution
+*   \param min_batch_size The minimum batch size for model execution
+*   \param tag A tag to attach to the model for information purposes
+*   \param tag_length The length of the tag string,
+*                     excluding null terminating character
+*   \param inputs One or more names of model input nodes (TF models only)
+*   \param input_lengths The length of each input name string,
+*                        excluding null terminating character
+*   \param n_inputs The number of inputs
+*   \param outputs One or more names of model output nodes (TF models only)
+*   \param output_lengths The length of each output name string,
+*                         excluding null terminating character
+*   \param n_outputs The number of outputs
+*   \return Returns SRNoError on success or an error code on failure
+*/
+SRError set_model_from_file_multigpu(void* c_client,
+                                     const char* name,
+                                     const size_t name_length,
+                                     const char* model_file,
+                                     const size_t model_file_length,
+                                     const char* backend,
+                                     const size_t backend_length,
+                                     const int first_gpu,
+                                     const int num_gpus,
+                                     const int batch_size,
+                                     const int min_batch_size,
+                                     const char* tag,
+                                     const size_t tag_length,
+                                     const char** inputs,
+                                     const size_t* input_lengths,
+                                     const size_t n_inputs,
+                                     const char** outputs,
+                                     const size_t* output_lengths,
+                                     const size_t n_outputs);
+/*!
 *   \brief Set a model (from buffer) in the database for future execution
 *   \details The final model key used to store the model
 *            may be formed by applying a prefix to the supplied
@@ -419,6 +474,78 @@ SRError set_model(void* c_client,
                   const char** outputs,
                   const size_t* output_lengths,
                   const size_t n_outputs);
+ 
+ /*!
+*   \brief Set a model (from buffer) in the database for future execution
+*   \details The final model key used to store the model
+*            may be formed by applying a prefix to the supplied
+*            name. Similarly, the tensor names in the
+*            input and output nodes for TF models may be prefixed.
+*            See set_data_source(), use_model_ensemble_prefix(), and
+*            use_tensor_ensemble_prefix() for more details
+*   \param c_client The client object to use for communication
+*   \param name The name to associate with the model
+*   \param name_length The length of the name string,
+*                      excluding null terminating character
+*   \param model The model as a continuous buffer
+*   \param model_length The length of the model string,
+*                       excluding null terminating character
+*   \param backend The name of the backend (TF, TFLITE, TORCH, ONNX)
+*   \param backend_length The length of the backend string,
+*                        excluding null terminating character
+*   \param first_gpu the first GPU (zero-based) to use with the model
+*   \param num_gpus The number of GPUs to use with the model
+*   \param batch_size The batch size for model execution
+*   \param min_batch_size The minimum batch size for model execution
+*   \param tag A tag to attach to the model for information purposes
+*   \param tag_length The length of the tag string,
+*                     excluding null terminating character
+*   \param inputs One or more names of model input nodes (TF models only)
+*   \param input_lengths The length of each input name string,
+*                        excluding null terminating character
+*   \param n_inputs The number of inputs
+*   \param outputs One or more names of model output nodes (TF models only)
+*   \param output_lengths The length of each output name string,
+*                         excluding null terminating character
+*   \param n_outputs The number of outputs
+*   \return Returns SRNoError on success or an error code on failure
+*/
+SRError set_model_multigpu(void* c_client,
+                  const char* name,
+                  const size_t name_length,
+                  const char* model,
+                  const size_t model_length,
+                  const char* backend,
+                  const size_t backend_length,
+                  const int first_gpu,
+                  const int num_gpus,
+                  const int batch_size,
+                  const int min_batch_size,
+                  const char* tag,
+                  const size_t tag_length,
+                  const char** inputs,
+                  const size_t* input_lengths,
+                  const size_t n_inputs,
+                  const char** outputs,
+                  const size_t* output_lengths,
+                  const size_t n_outputs);
+
+/*!
+*   \brief Get a model in the database. The memory associated with the
+*          model string is valid until the client is destroyed.
+*   \details The model key used to locate the model
+*            may be formed by applying a prefix to the supplied
+*            name. See set_data_source()
+*            and use_model_ensemble_prefix() for more details.
+*   \param c_client The client object to use for communication
+*   \param name The name to use to get the model
+*   \param name_length The length of the name string,
+*                      excluding null terminating character
+*   \param model_length The length of the model buffer string,
+*                       excluding null terminating character
+*   \param model Receives the model as a string
+*   \returns Returns SRNoError on success or an error code on failure
+*/                 const size_t n_outputs);
 
 /*!
 *   \brief Get a model in the database. The memory associated with the
@@ -471,6 +598,38 @@ SRError set_script_from_file(void* c_client,
                              const size_t script_file_length);
 
 /*!
+*   \brief Set a script from file in the database for future execution
+*   \details The final script key used to store the script
+*            may be formed by applying a prefix to the supplied
+*            name. See use_model_ensemble_prefix() for more details
+*   \param c_client The client object to use for communication
+*   \param name The name to associate with the script
+*   \param name_length The length of the name string,
+*                      excluding null terminating character
+*   \param device The name of the device for execution. May be either
+*                 CPU or GPU. If multiple GPUs are present, a specific
+*                 GPU can be targeted by appending its zero-based
+*                 index, i.e. "GPU:1"
+*   \param device_length The length of the device name string,
+*                        excluding null terminating character
+*   \param script_file The source file for the script
+*   \param script_file_length The length of the script file name string,
+*                             excluding null terminating character
+*   \param first_gpu the first gpu (zero-based) to use with the model
+*   \param num_gpus the number of gpus to use with the model
+*   \return Returns SRNoError on success or an error code on failure
+*/
+SRError set_script_from_file_multigpu(void* c_client,
+                                      const char* name,
+                                      const size_t name_length,
+                                      const char* device,
+                                      const size_t device_length,
+                                      const char* script_file,
+                                      const size_t script_file_length,
+                                      const int first_gpu,
+                                      const int num_gpus);
+
+/*!
 *   \brief Set a script (from buffer) in the database for future execution
 *   \details The final script key used to store the script
 *            may be formed by applying a prefix to the supplied
@@ -495,6 +654,31 @@ SRError set_script(void* c_client,
                    const size_t name_length,
                    const char* device,
                    const size_t device_length,
+                   const char* script,
+                   const size_t script_length);
+
+/*!
+*   \brief Set a script (from buffer) in the database for future execution
+*          execution in a multi-GPU system
+*   \details The final script key used to store the script
+*            may be formed by applying a prefix to the supplied
+*            name. See use_model_ensemble_prefix() for more details
+*   \param c_client The client object to use for communication
+*   \param name The name to associate with the script
+*   \param name_length The length of the name string,
+*                      excluding null terminating character
+*   \param first_gpu the first gpu (zero-based) to use with the model
+*   \param num_gpus the number of gpus to use with the model
+*   \param script The script as a string buffer
+*   \param script_length The length of the script string,
+*                        excluding null terminating character
+*   \return Returns SRNoError on success or an error code on failure
+*/
+SRError set_script_multigpu(void* c_client,
+                   const char* name,
+                   const size_t name_length,
+                   const int first_gpu,
+                   const int num_gpus,
                    const char* script,
                    const size_t script_length);
 
@@ -560,6 +744,51 @@ SRError run_script(void* c_client,
                    const size_t n_outputs);
 
 /*!
+*   \brief Run a script function in the database using the specificed input
+*          and output tensors in a multi-GPU system
+*   \details The script key used to locate the script to be run
+*            may be formed by applying a prefix to the supplied
+*            name. Similarly, the tensor names in the
+*            input and output arrays may be prefixed.
+*            See set_data_source(), use_model_ensemble_prefix(), and
+*            use_tensor_ensemble_prefix() for more details
+*   \param c_client The client object to use for communication
+*   \param name The name associated with the script
+*   \param name_length The length of the name string,
+*                      excluding null terminating character
+*   \param function The name of the function in the script to run
+*   \param function_length The length of the function name string,
+*                          excluding null terminating character
+*   \param inputs The tensor keys of inputs tensors to use in the script
+*   \param input_lengths The length of each input name string,
+*                        excluding null terminating character
+*   \param n_inputs The number of inputs
+*   \param outputs The tensor keys of output tensors that will be used
+*                  to save script results
+*   \param output_lengths The length of each output name string,
+*                         excluding null terminating character
+*   \param n_outputs The number of outputs
+*   \param offset index of the current image, such as a processor
+*                   ID or MPI rank
+*   \param first_gpu the first GPU (zero-based) to use with the script
+*   \param num_gpus the number of gpus for which the script was stored
+*   \return Returns SRNoError on success or an error code on failure
+*/
+SRError run_script_multigpu(void* c_client,
+                            const char* name,
+                            const size_t name_length,
+                            const char* function,
+                            const size_t function_length,
+                            const char** inputs,
+                            const size_t* input_lengths,
+                            const size_t n_inputs,
+                            const char** outputs,
+                            const size_t* output_lengths,
+                            const size_t n_outputs,
+                            const int offset,
+                            const int first_gpu,
+                            const int num_gpus);
+/*!
 *   \brief Run a model in the database using the specificed input and
 *          output tensors
 *   \details The model key used to locate the model to be run
@@ -590,6 +819,45 @@ SRError run_model(void* c_client,
                   const char** outputs,
                   const size_t* output_lengths,
                   const size_t n_outputs);
+
+/*!
+*   \brief Run a model in the database using the specificed input and
+*          output tensors in a multi-GPU system
+*   \details The model key used to locate the model to be run
+*            may be formed by applying a prefix to the supplied
+*            name. See set_data_source()
+*            and use_model_ensemble_prefix() for more details.
+*   \param c_client The client object to use for communication
+*   \param name The name associated with the model
+*   \param name_length The length of the name string,
+*                      excluding null terminating character
+*   \param inputs The names of inputs tensors to use in the script
+*   \param input_lengths The length of each input name string,
+*                        excluding null terminating character
+*   \param n_inputs The number of inputs
+*   \param outputs The names of output tensors to be used
+*                  to save script results
+*   \param output_lengths The length of each output name string,
+*                         excluding null terminating character
+*   \param n_outputs The number of outputs
+*   \param offset index of the current image, such as a processor
+*                 ID or MPI rank
+*   \param first_gpu the first GPU (zero-based) to use with the model
+*   \param num_gpus the number of gpus for which the script was stored
+*   \return Returns SRNoError on success or an error code on failure
+*/
+SRError run_model_multigpu(void* c_client,
+                  const char* name,
+                  const size_t name_length,
+                  const char** inputs,
+                  const size_t* input_lengths,
+                  const size_t n_inputs,
+                  const char** outputs,
+                  const size_t* output_lengths,
+                  const size_t n_outputs,
+                  const int offset,
+                  const int first_gpu,
+                  const int num_gpus);
 
 /*!
 *   \brief Remove a model from the database
