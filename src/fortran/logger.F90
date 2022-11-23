c19ae428850cb2e5c0006b1f88ddb8e79bad6749
billschereriii <bill.scherer@hpe.com>
2022-11-23 12:45:35 -0600

    It compiles so it must be fine

---
+++ src/fortran/logger.F90
@@ -0,0 +1,99 @@
+! BSD 2-Clause License
+!
+! Copyright (c) 2021-2022, Hewlett Packard Enterprise
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright notice, this
+!    list of conditions and the following disclaimer.
+!
+! 2. Redistributions in binary form must reproduce the above copyright notice,
+!    this list of conditions and the following disclaimer in the documentation
+!    and/or other materials provided with the distribution.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+! DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+! OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+module smartredis_logger
+
+use iso_c_binding, only : c_ptr, c_char, c_size_t
+use iso_c_binding, only : c_int8_t, c_int16_t, c_int32_t, c_int64_t, c_float, c_double
+use iso_c_binding, only : c_loc, c_f_pointer
+use fortran_c_interop, only : convert_char_array_to_c, enum_kind, C_MAX_STRING
+
+use, intrinsic :: iso_fortran_env, only: stderr => error_unit
+implicit none; private
+
+#include "enum_fortran.inc"
+#include "logger/logger_interfaces.inc"
+
+public :: enum_kind !< The kind of integer equivalent to a C enum. According to C an Fortran
+                    !! standards this should be c_int, but is renamed here to ensure that
+                    !! users do not have to import the iso_c_binding module into their
+                    !! programs
+
+contains
+
+!> Log data to the SmartRedis log
+subroutine log_data(level, data)
+  integer (kind=enum_kind), intent(in) :: level !< Minimum logging level to log the data at
+  character(len=*),         intent(in) :: data  !< Data to be logged
+
+  ! Local variables
+  character(kind=c_char, len=:), allocatable :: c_data
+  integer(kind=c_size_t) :: c_data_length
+
+  allocate(character(kind=c_char, len=len_trim(data)) :: c_data)
+  c_data = data
+  c_data_length = len_trim(data)
+
+  call c_log_data(level, c_data, c_data_length)
+  deallocate(c_data)
+end subroutine log_data
+
+!> Log a warning to the SmartRedis log
+subroutine log_warning(level, data)
+  integer (kind=enum_kind), intent(in) :: level !< Minimum logging level to log the data at
+  character(len=*),         intent(in) :: data  !< Data to be logged
+
+  ! Local variables
+  character(kind=c_char, len=:), allocatable :: c_data
+  integer(kind=c_size_t) :: c_data_length
+
+  allocate(character(kind=c_char, len=len_trim(data)) :: c_data)
+  c_data = data
+  c_data_length = len_trim(data)
+
+  call c_log_warning(level, c_data, c_data_length)
+  deallocate(c_data)
+end subroutine log_warning
+
+!> Log an error to the SmartRedis log
+subroutine log_error(level, data)
+  integer (kind=enum_kind), intent(in) :: level !< Minimum logging level to log the data at
+  character(len=*),         intent(in) :: data  !< Data to be logged
+
+  ! Local variables
+  character(kind=c_char, len=:), allocatable :: c_data
+  integer(kind=c_size_t) :: c_data_length
+
+  allocate(character(kind=c_char, len=len_trim(data)) :: c_data)
+  c_data = data
+  c_data_length = len_trim(data)
+
+  call c_log_error(level, c_data, c_data_length)
+  deallocate(c_data)
+end subroutine log_error
+
+end module smartredis_logger
+
