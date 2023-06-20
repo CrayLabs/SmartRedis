a9cc0865fcb579412fea5a192aa62058db4dcbe5
billschereriii <bill.scherer@hpe.com>
2023-06-20 16:58:35 -0500

    server launch script code complete

--- utils/launch_redis.py
+++ utils/launch_redis.py
@@ -24,24 +24,41 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from subprocess import Popen, TimeoutExpired, PIPE, SubprocessError, run
-from time import sleep
+from subprocess import Popen, SubprocessError, run
+import argparse
+import os
 
-def stop_cluster(n_nodes, port):
+def stop_db(n_nodes, port, udsport):
     """Stop a redis cluster and clear the files
     associated with it
     """
-    import os
-    redis = os.getenv('REDIS_INSTALL_PATH') + '/redis-cli'
+    # It's clobberin' time!
+    rediscli = os.getenv('REDIS_INSTALL_PATH')
+    if rediscli is not None:
+        rediscli += '/redis-cli'
+    else:
+        rediscli = os.path.abspath(
+            os.basedir(__file__) + "/../third-party/redis/src/redis-server"
+        )
+    is_uds = udsport is not None
 
-
-    pids = []
+    # Clobber the server(s)
+    procs = []
     for i in range(n_nodes):
-        cmd = redis + ' -p ' + str(port+i) + ' shutdown'
-        pid = Popen([cmd], shell=True)
-        pids.append(pid)
+        connection = f"-s udsport" if is_uds else f"-p {str(port+1)}"
+        cmd = f"{rediscli} {connection} shutdown"
+        proc = Popen(cmd, shell=True)
+        procs.append(proc)
 
-        sleep(1)
+    # Make sure that all servers are down
+    # Let exceptions propagate to the caller
+    for proc in procs:
+        _,_ = proc.communicate(timeout=15)
+        if proc.returncode != 0:
+            raise RuntimeError("Failed to launch Redis server!")
+
+    # clean up after ourselves
+    for i in range(n_nodes):
         fname = str(port+i) + ".log"
         if os.path.exists(fname):
             os.remove(fname)
@@ -50,64 +67,138 @@ def stop_cluster(n_nodes, port):
         if os.path.exists(fname):
             os.remove(fname)
 
-    fname = 'dump.rdb'
-    if os.path.exists(fname):
+    other_files = [
+        'dump.rdb',
+        'single.log',
+        'UDS.log',
+    ]
+    for fname in other_files:
+        if os.path.exists(fname):
             os.remove(fname)
 
-    return pids
+def prepare_uds_socket(udsport):
+    """Sets up the UDS socket
+    """
+    if udsport is not None:
+        uds_abs = os.path.abspath(udsport)
+        basedir = os.path.basedir(uds_abs)
+        if not os.path.exists(basedir):
+            os.makedir(basedir)
+        if not os.path.exists(uds_abs):
+            file(uds_abs, 'w+').close()
+        os.chmod(uds_abs, 0o777)
 
-def create_cluster(n_nodes, port):
-    """Creates a cluster starting with port at
-    127.0.0.1"""
+def create_db(n_nodes, port, device, rai_ver, udsport):
+    """Creates a redis database starting with port at 127.0.0.1
 
-    import os
+    For a standalone server, the command issued should be equivalent to:
+        redis-server --port $PORT --daemonize yes \
+                     --logfile "single.log" \
+                     --loadmodule $REDISAI_MODULES
+
+    For a clustered server, the command issued should be equivalent to:
+        redis-server --port $port --cluster-enabled yes --daemonize yes \
+                        --cluster-config-file "$port.conf" --protected-mode no --save "" \
+                        --logfile "$port.log" \
+                        --loadmodule $REDISAI_MODULES
+
+    For a UDS server, the command issued should be equivalent to:
+        redis-server --unixsocket $SOCKET --unixsocketperm 777 --port 0 --bind 127.0.0.1 \
+                    --daemonize yes --protected-mode no --logfile "uds.log" \
+                    --loadmodule $REDISAI_MODULES
+
+    where:
+        PORT ranges from port to port + n_nodes - 1
+        REDISAI_MODULES is read from the environment or calculated relative to this file
+    """
+
+    # Set up configuration
+    is_cluster = n_nodes > 1
+    is_uds = udsport is not None
+    redisserver = os.getenv('REDIS_INSTALL_PATH') + '/redis-server'
+    if redisserver is None:
+        redisserver = os.path.abspath(
+            os.basedir(__file__) + "/../third-party/redis/src/redis-server"
+        )
+    rediscli = os.path.basename(redisserver) + "/redis-cli"
+    test_device = device if device is not None else os.environ.get(
+        "SMARTREDIS_TEST_DEVICE","cpu").lower()
+    if rai_ver is not None:
+        redisai_dir = os.path.abspath(
+            os.basedir(__file__) + "/../third-party/RedisAI/" + rai_ver +
+            "install-" + test_device
+        )
+        redisai = redisai_dir + "/redisai.so"
+        tf_loc = redisai_dir + "backends/redisai_tensorflow/redisai_tensorflow.so"
+        torch_loc = redisai_dir + "backends/redisai_torch/redisai_torch.so"
+        rai_clause = f"--loadmodule {redisai} TF {tf_loc} TORCH {torch_loc}"
+    else:
+        redisai = os.getenv(f'REDISAI_{test_device.upper()}_INSTALL_PATH') + '/redisai.so'
+        redisai_modules = os.getenv("REDISAI_MODULES")
+        if redisai_modules is None:
+            raise RuntimeError("REDISAI_MODULES environment variable is not set!")
+        rai_clause = f"--loadmodule {redisai_modules}"
+    uds_clause = ""
+    if is_uds:
+        prepare_uds_socket(udsport)
+        uds_clause = f"--bind 127.0.0.1 --unixsocket {udsport} --unixsocketperm 777"
+    daemonize_clause = "--daemonize yes"
+    cluster_clause = "--cluster-enabled yes" if is_cluster else ""
+    prot_clause = "--protected-mode no" #if is_cluster or is_uds else ""
+    save_clause = "--save \"\"" #if is_cluster else ""
 
     # Start servers
-    host = '127.0.0.1'
-    redis = os.getenv('REDIS_INSTALL_PATH') + '/redis-server'
-    test_device = os.environ.get("SMARTREDIS_TEST_DEVICE","cpu").lower()
-    redisai = os.getenv(f'REDISAI_{test_device.upper()}_INSTALL_PATH') + '/redisai.so '
-    pids = []
-
+    procs = []
     for i in range(n_nodes):
         l_port = port + i
-        cmd = redis + ' --port ' + str(l_port) + " --cluster-enabled yes --cluster-config-file "  + str(l_port) + ".conf --loadmodule " + \
-            redisai + " --protected-mode no --loglevel notice "
-        log_file = "--logfile "  + str(l_port) + ".log"
-        cmd += log_file + ' '
+        port_clause = f"--port {str(l_port)}" if not is_uds else "--port 0"
+        if is_cluster:
+            log_clause = f"--logfile {str(l_port)}.log"
+            cluster_cfg_clause = f"--cluster-config-file {{str(l_port)}.conf}"
+        else:
+            log_clause = "--logfile " + ("UDS.log" if is_uds else "single.log")
+            cluster_cfg_clause = ""
+        log_clause += " --loglevel notice"
+        cmd = f"{redisserver} {port_clause} {daemonize_clause} {cluster_clause} " + \
+              f"{cluster_cfg_clause} {log_clause} {uds_clause} {rai_clause} " + \
+              f"{prot_clause} {save_clause}"
+
         print(cmd)
-        pid = Popen(cmd, shell=True)
-        pids.append(pid)
-    sleep(2)
-    # Create cluster
+        proc = Popen(cmd, shell=True)
+        procs.append(proc)
+
+    # Make sure that all servers are up
+    # Let exceptions propagate to the caller
+    for proc in procs:
+        _,_ = proc.communicate(timeout=15)
+        if proc.returncode != 0:
+            raise RuntimeError("Failed to launch Redis server!")
+
+    # Create cluster for clustered Redis request
     if n_nodes > 1:
-        redis_cli = os.getenv('REDIS_INSTALL_PATH') + '/redis-cli'
         cluster_str=' '
         for i in range(n_nodes):
             cluster_str += '127.0.0.1:' + str(port+i) + ' '
-        cmd = " ".join((redis_cli, "--cluster create", cluster_str, "--cluster-replicas 0"))
+        cmd = f"{rediscli} --cluster create {cluster_str} --cluster-replicas 0"
         print(cmd)
         proc = run([cmd], input="yes", encoding="utf-8", shell=True)
         if proc.returncode != 0:
             raise SubprocessError("Cluster could not be created!")
-        else:
-            print("Cluster has been setup!")
+        print("Cluster has been setup!")
     else:
         print("Server has been setup!")
 
-    return pids
-
 if __name__ == "__main__":
-    import argparse
-    import time
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=6379)
     parser.add_argument('--nodes', type=int, default=3)
+    parser.add_argument('--rai', type=str, default=None)
+    parser.add_argument('--udsport', type=str, default=None)
+    parser.add_argument('--device', type=str, default="cpu")
     parser.add_argument('--stop', action='store_true')
     args = parser.parse_args()
 
-    if(args.stop):
-        stop_cluster(args.nodes, args.port)
+    if args.stop:
+        stop_db(args.nodes, args.port, args.udsport)
     else:
-        create_cluster(args.nodes, args.port)
+        create_db(args.nodes, args.port, args.device, args.rai, args.udsport)
